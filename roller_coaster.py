3af0fcc39ab374f5d3621b0ca965da049058a5f0
Aniketh <167979232+Anikxth-11@users.noreply.github.com>
2024-06-03 15:28:45 +0530

    Add files via upload

---
+++ roller_coaster.py
@@ -0,0 +1,11 @@
+print('WELCOME TO THE ROLLER COASTER RIDE!!')
+height=int(input('please enter your height in centimeters:\n'))
+if height > 120:
+    print('you are allowed to enter the rollercoaster ride\n please collect the tickets from the next counter!!')
+else:
+    print('sorry you are not allowed to enter beacuse the height requirement is not fulfilled\n\nTHANK YOU VISIT AGAIN!!')
+
+
+    
+
+
